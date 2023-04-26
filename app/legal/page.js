71ef5617746f5f17060a99e63a5e672b165789d8
Zainan Victor Zhou <zzn@zzn.im>
2023-04-26 11:44:32 -0700

    chore: add legal info

---
+++ app/legal/page.js
@@ -0,0 +1,10 @@
+export default function Legal() {
+    return (
+      <main>
+        <p>EthGPT is a plugin to bring Ethereum and EVM-chain based dApp and smart contract functionalities to ChatGPT.</p>
+        <p>The early development was supported by Zainan Victor Zhou and team of D3Serve Labs Inc and as an open source project.</p>
+        <p>The source code can be found in the <a href="https://github.com/xinbenlv/ethgpt">Github repo</a></p>
+        <p>It is still super early, please expect bugs and contributions are welcomed.</p>
+      </main>
+    )
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <p>Visit <a href="https://platform.openai.com/docs/plugins/introduction" target="_blank" rel="noopener">the ChatGPT Plugin Docs</a> to learn more about ChatGPT Plugins.</p>
+      API for EthGPT ChatGPT Plugins
     </main>
   )
 }
